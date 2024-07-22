a123a6dc9802bba7d587260058efa8bcc7fbb368
Oxygen 🌏 <161521423+OxygenRoblox@users.noreply.github.com>
2024-07-22 14:54:59 -0400

    Update MultiInstance.sh

--- MultiInstance.sh
+++ MultiInstance.sh
@@ -38,8 +38,11 @@ ROBLOX_PLAYER_PATH="/Applications/Roblox.app/Contents/MacOS/RobloxPlayer"
 
 # Launch Roblox Player and capture its console output
 echo "Launching Roblox Player..."
+echo "Norby is a Faggot Skid :D"
 "$ROBLOX_PLAYER_PATH" 2>&1 | while IFS= read -r line; do
     if [[ "$line" == \[IPC\]* ]]; then
         log_ipc_message "$(echo "$line" | sed 's/^\[IPC\]//')"
     fi
 done
+# Base Line End Stack For RobloxPlayer Seems like norb is a faggot who hates on anything.
+
