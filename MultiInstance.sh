9374bdbbd97e3e09b1479312ba3e61f38f064f98
Oxygen 🌏 <161521423+OxygenRoblox@users.noreply.github.com>
2024-08-21 19:51:25 -0400

    Update MultiInstance.sh

--- MultiInstance.sh
+++ MultiInstance.sh
@@ -1,74 +1,13 @@
 #!/bin/bash
 
-# Display [Raptor] Development LLC info and version
-echo "[Raptor] Development LLC"
-echo "    -> Multi Instance <.> V.1.0.1"
-echo
+# Path to the file to be updated
+FILE_PATH="/Users/abdifatah/Downloads/roblox_session.txt"
 
-# Display initial decorative lines
-echo "
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣾⣿⣿⣷⣶⣦⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡄⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⣿⣿⣿⡿⠟⠛⠛⠛⠛⠻⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⣾⣿⣿⣿⣿⣁⠀⠀⠀⠀⣀⣤⣶⣿⣿⣿⣿⣧⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⢰⣿⣿⣿⠛⠉⠛⠶⠀⠀⢐⠿⠋⠀⢨⣿⣿⣿⣿⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⢷⣿⣿⣶⠀⠀⠉⢶⣿⣿⠿⢿⣿⣿⣿⡄⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⢸⣿⣿⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⣿⣿⣿⡇⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣶⡶⠂⠀⣀⠀⢀⡄⠐⢲⡾⣻⣿⣿⣿⠇⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⣯⢿⡶⣶⣿⣟⣿⡶⠶⣿⢣⣿⣿⣿⣿⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⣀⣾⣿⣿⣿⣧⠛⠒⠠⣤⣤⠶⠾⢣⣿⣿⣿⣿⣿⣤⣀⠀⠀⠀
-⢀⣠⣤⣶⣶⣿⣿⣿⣿⣿⣿⣿⣿⣷⡄⠀⢿⣿⠀⣰⣿⣿⣿⣿⣿⣿⣿⣷⣶
-⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣾⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
-⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
-⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
-⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
-⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
-" 
+# Prompt user for input
+read -p "Enter your account cookie: " COOKIE
 
-# Define file paths
-COOKIE_JSON="/Users/abdifatah/Downloads/cookies.json"
-SESSION_FILE="/Users/abdifatah/Downloads/roblox_session.txt"
+# Update the file with the provided cookie
+echo "$COOKIE" > "$FILE_PATH"
 
-# Create COOKIE_JSON file with initial content if it does not exist
-if [ ! -f "$COOKIE_JSON" ]; then
-    echo '{"cookies": [{"cookie": "placeholder_cookie_value"}]}' > "$COOKIE_JSON"
-    echo "Created $COOKIE_JSON with initial content."
-fi
-
-# Function to get the current cookie from the JSON file
-get_current_cookie() {
-    jq -r '.cookies[0].cookie' "$COOKIE_JSON"
-}
-
-# Function to update the session file with the current cookie
-update_session_file() {
-    local cookie="$1"
-    echo "$cookie" > "$SESSION_FILE"
-}
-
-# Function to cycle through cookies
-cycle_cookies() {
-    local cookies=($(jq -r '.cookies[].cookie' "$COOKIE_JSON"))
-    local num_cookies=${#cookies[@]}
-    local current_index=0
-
-    while true; do
-        # Get the current cookie based on index
-        local cookie=${cookies[$current_index]}
-        update_session_file "$cookie"
-
-        # Move to the next cookie index
-        current_index=$(( (current_index + 1) % num_cookies ))
-
-        # Update the JSON file with the new cookie
-        local new_cookie=${cookies[$current_index]}
-        jq --arg new_cookie "$new_cookie" '.cookies[0].cookie = $new_cookie' "$COOKIE_JSON" > "${COOKIE_JSON}.tmp" && mv "${COOKIE_JSON}.tmp" "$COOKIE_JSON"
-
-        # Delay before updating again
-        sleep 5  # Adjust the delay as needed
-    done
-}
-
-# Start cycling through cookies
-cycle_cookies
+# Print a confirmation message
+echo "The file has been updated with the new cookie."
